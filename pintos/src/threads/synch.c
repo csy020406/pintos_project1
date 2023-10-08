0bbe42f4793a2665117bb1783d6d7cbd29f5e054
csy020406 <csy020406@postech.ac.kr>
2023-10-09 00:06:05 +0900

    주석2

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -68,13 +68,13 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      // list_insert_ordered (&sema->waiters, &thread_current ()->elem, thread_compare_priority, NULL);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, thread_compare_priority, NULL);
       thread_block ();
     }
   sema->value--;
   intr_set_level (old_level);
 
-  // printf("sema_down\n");  //debug
+  printf("sema_down\n");  //debug
 }
 
 /* Down or "P" operation on a semaphore, but only if the
@@ -117,7 +117,7 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters))
   {
-    list_sort (&sema->waiters, thread_compare_priority, NULL);
+    // list_sort (&sema->waiters, thread_compare_priority, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
   }
